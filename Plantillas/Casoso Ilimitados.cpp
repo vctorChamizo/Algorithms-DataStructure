9eb4b22256926b8f7dc0c3886c4610296756af24
Víctor Chamizo <32466953+vctorChamizo@users.noreply.github.com>
2018-09-21 10:26:15 +0200

    Plantilla Problemas

---
+++ Plantillas/Casoso Ilimitados.cpp
@@ -0,0 +1,45 @@
+// Nombre del alumno Víctor Chamizo Rodríguez
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+
+// función que resuelve el problema
+TipoSolucion resolver(TipoDatos datos) {
+    
+    
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    
+    if (! std::cin)
+        return false;
+    
+    TipoSolucion sol = resolver(datos);
+   
+    return true;   
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    #ifndef DOMJUDGE
+     std::ifstream in("datos.txt");
+     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+     #endif 
+    
+    while (resuelveCaso());
+	
+    // Para restablecer entrada. Comentar para acepta el reto
+     #ifndef DOMJUDGE // para dejar todo como estaba al principio
+     std::cin.rdbuf(cinbuf);
+     system("PAUSE");
+     #endif
+    
+    return 0;
+}
---
+++ Plantillas/Numero casos.cpp
@@ -0,0 +1,44 @@
+// Nombre del alumno: Víctor Chamizo Rodríguez
+// Usuario del Juez:
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+
+// función que resuelve el problema
+TipoSolucion resolver(TipoDatos datos) {
+    
+    
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+
+    TipoSolucion sol = resolver(datos);
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    #ifndef DOMJUDGE
+     std::ifstream in("datos.txt");
+     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+     #endif 
+    
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+	
+    // Para restablecer entrada. Comentar para acepta el reto
+     #ifndef DOMJUDGE // para dejar todo como estaba al principio
+     std::cin.rdbuf(cinbuf);
+     system("PAUSE");
+     #endif
+    
+    return 0;
+}
