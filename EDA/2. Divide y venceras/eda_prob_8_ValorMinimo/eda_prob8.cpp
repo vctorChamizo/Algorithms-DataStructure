68cee50339402638fe6ee949cac1ab63b4131d3f
Víctor Chamizo <32466953+vctorChamizo@users.noreply.github.com>
2018-10-03 23:11:58 +0200

    Solución eda_prob8

---
+++ EDA/2. Divide y venceras/eda_prob_8_ValorMinimo/eda_prob8.cpp
@@ -0,0 +1,63 @@
+﻿// Nombre del alumno Víctor Chamizo Rodríguez
+// Usuario del Juez E12
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+
+int calcularMinimo(std::vector<int> const& v, int ini, int fin) {
+
+	if (ini == fin) return v[ini];
+	else if (ini + 1 == fin) return std::min(v[ini], v[fin]);
+	else {
+
+		int m = (ini + fin) / 2;
+
+		if (v[m] < v[m - 1] && v[m] < v[m + 1]) return v[m];
+		else if (v[m] > v[ini]) return calcularMinimo(v, ini, m - 1);
+		else return calcularMinimo(v, m + 1, fin);
+	}
+}
+
+
+bool resuelveCaso() {
+
+	int nElem;
+
+	std::cin >> nElem;
+
+	if (!std::cin)
+		return false;
+
+	std::vector<int> vector(nElem);
+
+	for (int i = 0; i < vector.size(); ++i) 
+		std::cin >> vector[i];
+
+	int minimo = calcularMinimo(vector, 0, vector.size() - 1);
+
+	std::cout << minimo << std::endl;
+
+	return true;
+}
+
+int main() {
+
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif 
+
+	while (resuelveCaso());
+
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
