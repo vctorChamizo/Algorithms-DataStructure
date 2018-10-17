f557786a989a8369f5ffb7d77672a2f8debc68d7
Víctor Chamizo <32466953+vctorChamizo@users.noreply.github.com>
2018-10-17 11:37:31 +0200

    Solución tas_prob30

---
+++ TAIS/3. Grafos y estructuras de partición/tais_prob30_Amigos/tais_prob30.cpp
@@ -0,0 +1,55 @@
+﻿// Nombre del alumno: Víctor Chamizo Rodríguez
+// Usuario del Juez: TAIS58
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include "Grafo.h"
+#include "MaximaComponenteConexa.h"
+
+
+void resuelveCaso() {
+
+	int nPersonas; //Vertices del grafo
+	int paresAmigos; //Aristas del grafo
+
+	std::cin >> nPersonas >> paresAmigos;
+
+	Grafo grafo(nPersonas); //Inicializamos el grafo con el numero de vertices que va a tener.
+
+	int amigo1, amigo2;
+
+	for (int i = 0; i < paresAmigos; ++i) {
+
+		std::cin >> amigo1 >> amigo2;
+
+		//Con esta función colcamos las aristas con los vertices correspondientes.
+		grafo.ponArista(amigo1 - 1, amigo2 - 1); 
+	}
+
+	MaximaComponenteConexa mcc(grafo);
+
+	std::cout << mcc.maximo() << std::endl;
+}
+
+int main() {
+
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif 
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
