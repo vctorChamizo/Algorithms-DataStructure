d92f8447f003eeb42cae82187128e116b716ce64
Víctor Chamizo <32466953+vctorChamizo@users.noreply.github.com>
2018-10-29 13:22:25 +0100

    Solucion tais_prob33

---
+++ TAIS/3. Grafos y estructuras de partición/tais_prob33_Ovejas/Ovejas.h
@@ -0,0 +1,57 @@
+﻿#include <vector>
+
+using Imagen = std::vector<std::vector<char>>;
+
+class Ovejas {
+
+public:
+
+	/*
+		- Inicializamos el valor de las filas y columnas.
+		- El vector de marcado también es una matriz, que inicializamos a falso.
+		- El numero de manchas blancas inicializa a cero.
+	*/
+	Ovejas(Imagen const & I) :  F(I.size()), C(I[0].size()), marcado(F, std::vector<bool>(C, false)), manchasBlancas(0) {
+
+		//Comparabamos cada uno de los elementos de la matriz.
+		for (int i = 0; i < F; ++i) {
+			for (int j = 0; j < C; ++j) {
+				//Encontramos una nueva mancha blanca que no está marcada.
+				if (I[i][j] == '.' && !marcado[i][j]) {
+
+					manchasBlancas++;
+
+					dfs(I, i, j);
+				}
+			}
+				
+		}
+	}
+
+	int getOvejasBlancas() const { return manchasBlancas - 1; }
+
+
+private:
+
+	int F, C; //Dimensiones de la imagen
+	std::vector<std::vector<bool>> marcado; // marcado[i][j] = se ha visitado <i,j>
+	int manchasBlancas; // número de manchas blancas
+
+	// Creamos un vector de un vector de pares. Su finalidad es mirar los elemntos colindantes del elemento de la matriz
+	const std::vector<std::pair<int, int>> direcciones = { {1,0},{0,1},{-1,0},{0,-1} };
+
+	//Verifica que el elemento al que se quiere acceder esta dentro de los limites de la matriz.
+	bool correcta(int i, int j) const { return i >= 0 && i < F && j >= 0 && j < C; }
+
+	void dfs(Imagen const & I, int i, int j) {
+
+		marcado[i][j] = true;
+
+		for (auto d : direcciones) {
+
+			int ni = i + d.first, nj = j + d.second;
+
+			if (correcta(ni, nj) && I[ni][nj] == '.' && !marcado[ni][nj]) dfs(I, ni, nj);
+		}
+	}
+};
---
+++ TAIS/3. Grafos y estructuras de partición/tais_prob33_Ovejas/tais_prob33.cpp
@@ -0,0 +1,53 @@
+﻿// Nombre del alumno: Víctor Chamizo Rodríguez
+// Usuario del Juez: TAIS58
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Ovejas.h"
+
+
+bool resuelveCaso() {
+
+	int C;
+
+	std::cin >> C;
+
+	if (!std::cin)
+		return false;
+
+	int F;
+
+	std::cin >> F;
+
+	Imagen imagen(F, std::vector<char>(C));
+
+	for (int i = 0; i < F; ++i)
+		for (int j = 0; j < C; ++j)
+			std::cin >> imagen[i][j];
+	
+	Ovejas ovejas(imagen);
+
+	std::cout << ovejas.getOvejasBlancas();
+	
+	std::cout << std::endl;
+
+	return true;
+}
+
+int main() {
+
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif 
+
+	while (resuelveCaso());
+
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
