2af4b52c8ed49028dee6d49e0fd7623a1f9e1063
Víctor Chamizo <32466953+vctorChamizo@users.noreply.github.com>
2018-10-30 21:47:04 +0100

    Solucion tais_prob34

---
+++ TAIS/3. Grafos y estructuras de partición/tais_prob34_Petrolero/Manchas.h
@@ -0,0 +1,89 @@
+﻿// Nombre del alumno: Víctor Chamizo Rodríguez
+// Usuario del Juez: TAIS58
+
+#include <vector>
+#include <algorithm>
+
+#include "ConjuntosDisjuntos.h"
+
+using Area = std::vector<std::vector<char>>;
+
+class Manchas {
+
+public:
+
+	Manchas(Area & A) :  
+		F(A.size()),
+		C(A[0].size()),
+		marcado(F, std::vector<bool>(C, false)),
+		nManchas(0),
+		maximo(0),
+		conjuntos(F * C){
+
+		for (int i = 0; i < F; ++i)
+			for (int j = 0; j < C; ++j) 
+				if (A[i][j] == '#' && !marcado[i][j]) {
+
+					int size_m = 0;
+
+					nManchas++;
+					
+					dfs(A, i, j, size_m);
+
+					maximo = std::max(maximo, size_m);
+				}
+	}
+
+	int getManchas() const { return nManchas; }
+
+	int getMaximo() const { return maximo; }
+
+	void meterMancha(Area & A, int i, int j) {
+
+		A[i][j] = '#';
+
+		for (auto d : direcciones) {
+
+			int ni = i + d.first, nj = j + d.second;
+
+			if (correcta(ni, nj) && A[ni][nj] == '#') 
+				conjuntos.unir(getCoordenada(i, j), getCoordenada(ni, nj));
+		}
+
+		maximo = std::max(maximo, conjuntos.tam(getCoordenada(i, j)));
+	}
+
+private:
+
+	int F, C; //Dimensiones del area.
+	std::vector<std::vector<bool>> marcado; // marcado[i][j] = se ha visitado <i,j>
+	int nManchas, maximo;
+
+	ConjuntosDisjuntos conjuntos;
+
+	const std::vector<std::pair<int, int>> direcciones = { {1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1} };
+
+	bool correcta(int i, int j) const { return i >= 0 && i < F && j >= 0 && j < C; }
+
+	//Determina la posición unica que mantiene el conjunto disjunto dentro de la matriz.
+	int getCoordenada(int i, int j) const { return i * C + j; }
+
+	void dfs(Area const & A, int i, int j, int & size_m) {
+
+		marcado[i][j] = true;
+
+		size_m++;
+
+		for (auto d : direcciones) {
+
+			int ni = i + d.first, nj = j + d.second;
+
+			if (correcta(ni, nj) && A[ni][nj] == '#' && !marcado[ni][nj]) {
+
+				conjuntos.unir(getCoordenada(i, j), getCoordenada(ni, nj));
+
+				dfs(A, ni, nj, size_m);
+			}
+		}
+	}
+};
---
+++ TAIS/3. Grafos y estructuras de partición/tais_prob34_Petrolero/tais_prob34.cpp
@@ -0,0 +1,82 @@
+﻿// Nombre del alumno: Víctor Chamizo Rodríguez
+// Usuario del Juez: TAIS58
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+#include "Manchas.h"
+
+
+bool resuelveCaso() {
+
+	int F;
+
+	std::cin >> F;
+
+	if (!std::cin)
+		return false;
+
+	int C;
+
+	std::cin >> C;
+
+	std::cin.ignore();// Ignoramos el primer espacio que consume.
+
+	Area area(F, std::vector<char>(C));
+	std::string lectura;
+	char elem_aux;
+
+	for (int i = 0; i < F; ++i) {
+
+		getline(std::cin, lectura);// Leemos la linea entera.
+		
+		for (int j = 0; j < C; ++j) {//Recorremos cada uno de los elementos de la linea.
+
+			elem_aux = (j >= lectura.size() ? ' ' : lectura[j]);
+
+			area[i][j] = elem_aux;
+		}
+	}
+		
+	Manchas manchas(area);
+
+	std::cout << manchas.getMaximo();
+
+	int nCasos;
+
+	std::cin >> nCasos;
+
+	int x, y;
+
+	for (int i = 0; i < nCasos; ++i) {
+
+		std::cin >> x >> y;
+
+		manchas.meterMancha(area, x - 1, y - 1);
+
+		std::cout << " " << manchas.getMaximo();
+	}
+		
+	std::cout << std::endl;
+
+	return true;
+}
+
+int main() {
+
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif 
+
+	while (resuelveCaso());
+
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
